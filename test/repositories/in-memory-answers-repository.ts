de6147ca159092ad9282a9394a56cd16c4ca2043
Bertoldo Klinger <bertoldokbrj@gmail.com>
2024-03-03 21:03:40 -0300

    feat: add delete-answer use case and update answers-repository

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,7 +1,23 @@
-import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repositories'
+import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
 export class InMemoryAnswersRepository implements AnswersRepository {
+  async findById(id: string): Promise<Answer | null> {
+    const answer = this.items.find((item) => id === item.id.toString())
+
+    if (!answer) {
+      throw new Error('Answer not found')
+    }
+
+    return answer
+  }
+
+  async delete(answer: Answer): Promise<void> {
+    const itemIndex = this.items.findIndex((item) => item.id === answer.id)
+
+    this.items.splice(itemIndex, 1)
+  }
+
   public items: Answer[] = []
 
   async create(answer: Answer) {
