0ac4a42141ecd8544eb33a1fb3f1b34399129dbe
Bertoldo Klinger <bertoldokbrj@gmail.com>
2024-03-05 14:14:40 -0300

    feat: add delete functionality for question comments

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -9,4 +9,22 @@ export class InMemoryQuestionCommentsRepository
   async create(questionComment: QuestionComment) {
     this.items.push(questionComment)
   }
+
+  async findById(id: string): Promise<QuestionComment | null> {
+    const questionComment = this.items.find((item) => id === item.id.toString())
+
+    if (!questionComment) {
+      throw new Error('Answer not found')
+    }
+
+    return questionComment
+  }
+
+  async delete(questionComment: QuestionComment): Promise<void> {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === questionComment.id,
+    )
+
+    this.items.splice(itemIndex, 1)
+  }
 }
